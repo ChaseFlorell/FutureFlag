75aae23cae58df9035685f059afcfc11c829b7a0
Chase Florell <chase.florell@gmail.com>
2019-02-02 22:42:01 -0800

    [CF] testing unit test project

---
+++ tests/FutureFlag.Tests/VersionFutureFlagTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FutureFlag.Tests
+{
+    [TestFixture]
+    public class VersionFutureFlagTests
+    {
+        [Test]
+        public void Should()
+        {
+            
+        }
+    }
+}
