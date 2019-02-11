6e94bac1976c46cafabc5d97de421ccc41f0d410
Chase Florell <chase.florell@gmail.com>
2019-02-10 16:43:33 -0800

    [CF] added a CachedFutureFlagProvider. Fixes #9

---
+++ src/FutureFlag.Shared/CacheFutureFlagProvicer.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace FutureFlag
+{
+    /// <summary>
+    /// Defines an <see cref="IFutureFlag"/> whereby we can cache the child Flag for a given duration
+    /// </summary>
+    /// <inheritdoc cref="IFutureFlag"/>
+    public class CachedFutureFlagProvider : IFutureFlag
+    {
+        private DateTime _checkTime;
+        private bool _cachedValue;
+        
+        /// <summary>
+        /// Future Flag to cache
+        /// </summary>
+        public IFutureFlag FutureFlag { get; set; }
+        
+        /// <summary>
+        /// Duration to retain the cached value before re-checking the <see cref="FutureFlag"/>
+        /// </summary>
+        public TimeSpan CacheDuration { get; set; }
+
+        ///<inheritdoc cref="IFutureFlag.IsEnabled"/>
+        public bool IsEnabled => DateTime.Now >= _checkTime.Add(CacheDuration) 
+            ? UpdateCheckTimeAndReturnFlagValue() 
+            : _cachedValue;
+
+        private bool UpdateCheckTimeAndReturnFlagValue()
+        {
+            if(FutureFlag is null)
+                throw new InvalidOperationException($"You must provide an {nameof(IFutureFlag)} when using a {nameof(CachedFutureFlagProvider)}");
+            
+            if(CacheDuration == default(TimeSpan))
+                throw new InvalidOperationException($"You must provide a {nameof(CacheDuration)} when using a {nameof(CachedFutureFlagProvider)}");
+            
+            _checkTime = DateTime.Now;
+            return _cachedValue = FutureFlag.IsEnabled;
+        }
+    }
+}
---
+++ tests/FutureFlag.Tests/CachedFutureFlagProviderTests.cs
@@ -0,0 +1,78 @@
+using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace FutureFlag.Tests
+{
+    [TestFixture]
+    public class CachedFutureFlagProviderTests
+    {
+        [Test]
+        public void ShouldCacheAFutureFlagValue()
+        {
+           // setup
+           const int secondsToTest = 1;
+           var loopCount = 0;
+           var simpleFlag = new Mock<IFutureFlag>();
+           simpleFlag.SetupGet(x => x.IsEnabled).Returns(true);
+           var cacheProvider = new CachedFutureFlagProvider
+           {
+               FutureFlag = simpleFlag.Object,
+               CacheDuration = TimeSpan.FromSeconds(secondsToTest)
+           };
+           var stamp = DateTime.Now;
+           var end = DateTime.Now.AddSeconds(secondsToTest);
+           
+           // assert
+           cacheProvider.IsEnabled.Should().BeTrue("We manually set IsEnabled to true");
+           
+           // execute
+           simpleFlag.SetupGet(x => x.IsEnabled).Returns(false);
+           
+           // assert
+           while (end > stamp)
+           {
+               stamp = DateTime.Now;
+               cacheProvider.IsEnabled.Should().BeTrue("even though we've set it to false, the true value is cached");
+               loopCount++;
+           }
+
+           loopCount.Should().BeGreaterThan(100, because: "the loop should execute many times in one second");
+           cacheProvider.IsEnabled.Should().BeFalse("the cache has now expired and we should be picking up the new value");
+        }
+
+        [Test]
+        public void ShouldThrowInvalidOperationExceptionWhenMissingCacheDuration()
+        {
+            // setup
+            var mockFlag = new Mock<IFutureFlag>();
+            var cacheProvider = new CachedFutureFlagProvider
+            {
+                FutureFlag = mockFlag.Object
+            };
+            
+            // execute
+            Func<bool> result = () => cacheProvider.IsEnabled;
+            
+            // assert
+            result.Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void ShouldThrowInvalidOperationExceptionWhenMissingFutureFlag()
+        {
+            // setup
+            var cacheProvider = new CachedFutureFlagProvider
+            {
+                CacheDuration = TimeSpan.Zero
+            };
+            
+            // execute
+            Func<bool> result = () => cacheProvider.IsEnabled;
+            
+            // assert
+            result.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
